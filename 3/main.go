be2416e9debfb552b90fd52e1b46d629061db732
global <asgagagsad@gmail.com>
2022-07-17 00:55:31 +0300

    1

---
+++ 3/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"unicode"
+)
+
+// nextFunc returns the next word from the generator
+type nextFunc func() string
+
+// counter stores the number of digits in each word.
+// each key is a word and value is the number of digits in the word.
+type counter map[string]int
+
+// pair stores a word and the number of digits in it
+type pair struct {
+	word  string
+	count int
+}
+
+// countDigitsInWords counts digits in words,
+// fetching each word with the next() function
+func countDigitsInWords(next nextFunc) counter {
+	counted := make(chan pair)
+
+	// начало решения
+
+	go func() {
+		// Пройдите по словам,
+		// посчитайте количество цифр в каждом,
+		// и запишите его в канал counted
+	}()
+
+	// Считайте значения из канала counted
+	// и заполните stats.
+
+	// В результате stats должна содержать слова
+	// и количество цифр в каждом.
+
+	// конец решения
+
+	return stats
+}
+
+// countDigits returns the number of digits in a string
+func countDigits(str string) int {
+	count := 0
+	for _, char := range str {
+		if unicode.IsDigit(char) {
+			count++
+		}
+	}
+	return count
+}
+
+// printStats prints words and their digit counts
+func printStats(stats counter) {
+	for word, count := range stats {
+		fmt.Printf("%s: %d\n", word, count)
+	}
+}
+
+// wordGenerator returns a generator,
+// which emits words from a phrase.
+func wordGenerator(phrase string) nextFunc {
+	words := strings.Fields(phrase)
+	idx := 0
+	return func() string {
+		if idx == len(words) {
+			return ""
+		}
+		word := words[idx]
+		idx++
+		return word
+	}
+}
+
+func main() {
+	phrase := "0ne 1wo thr33 4068"
+	next := wordGenerator(phrase)
+	stats := countDigitsInWords(next)
+	printStats(stats)
+}
